436db7dc95cd389b52a3a13b3421e840092a04a6
Henry <hentra.37@gmail.com>
2022-10-06 17:44:40 -0500

    Added characteristics table and endpoints

--- profile/migrations/002_my_characteristics_table.py
+++ profile/migrations/002_my_characteristics_table.py
@@ -3,12 +3,12 @@ steps = [
         ## Create the table
         """
         CREATE TABLE characteristics (
+            id SERIAL PRIMARY KEY NOT NULL,
             DOB DATE NOT NULL,
             dog_friendly SMALLINT NOT NULL,
             well_trained SMALLINT NOT NULL,
             energy_level SMALLINT NOT NULL,
-            playfulness SMALLINT NOT NULL,
-            profile_id INTEGER REFERENCES profile("id") ON DELETE CASCADE
+            playfulness SMALLINT NOT NULL
         );
         """,
         ## Drop the table
