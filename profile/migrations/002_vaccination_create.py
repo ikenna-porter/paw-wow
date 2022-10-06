7d68b025a4a41a9c50fa74b5b4a447ac4938ae28
Sorena Sawyer <sorenasawyer@gmail.com>
2022-10-06 15:26:29 -0400

    created new migration for vaccination tables

---
+++ profile/migrations/002_vaccination_create.py
@@ -0,0 +1,19 @@
+steps = [
+    ## Create a vaccination table
+    """
+    CREATE TABLE vaccination (
+        id SERIAL PRIMARY KEY NOT NULL,
+        profile_id INTEGER REFERENCES profile("id") ON DELETE CASCADE,
+        distempeter BOOL NULL,
+        parvo BOOL NULL,
+        adeno BOOL NULL,
+        rabies BOOL NULL,
+        other VARCHAR(100) NULL
+    );
+    """,
+
+    ## Drop a vaccination table
+    """
+    DROP TABLE vaccination;
+    """
+]
