7ac43a230e7de7acfadf89c4d8fc7138bddbbffb
Jason Olefson <jasonolefson@gmail.com>
2022-10-13 15:50:22 -0400

    friendship table created & create requesty  working

--- profile/migrations/009_friendship_table.py
+++ profile/migrations/009_friendship_table.py
@@ -3,10 +3,10 @@ steps = [
         ## Create a friendships table
         """
         CREATE TABLE friendships (
-            friendship_id SERIAL PRIMARY KEY NOT NULL,
-            friendship_status INT NOT NULL,
+            id SERIAL PRIMARY KEY NOT NULL,
+            status INT NOT NULL,
             user_one INTEGER REFERENCES profiles("id") NOT NULL,
-            user_two INTEGER REFERENCES profiles("id") NOT NULL,
+            user_two INTEGER REFERENCES profiles("id") NOT NULL
         );
         """,
         ## Drop friendships table
