4f3cdd651f6cc912d19fe78c6a4ebc5d16f94508
Henry <hentra.37@gmail.com>
2022-10-19 23:34:34 -0500

    Working websocket, notification added to nav

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,10 +1,20 @@
 import { NavLink, Link } from 'react-router-dom';
+import Notifications from "./Images/Notifications.svg"
+import "./Nav.css"
 
 export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-success">
             <div className="container-fluid">
-                <NavLink className="navbar-brand" to="/">Paw Wow</NavLink>
+                <NavLink className="navbar-brand" to="/">
+                    Paw Wow
+                </NavLink>
+                <div className="icons">
+                    <div className="icon">
+                        <img src={Notifications} className="iconImg" alt=""/>
+                        <div className="counter">1 </div>
+                </div>
+            </div>
             </div>
         </nav>        
     )
